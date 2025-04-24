ebe0ccc3890edde35ca86b6e6b5050a984d17f49
LavaPremium <maksimlaktionov372@gmail.com>
2025-04-24 18:06:37 +0400

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Unity.VisualScripting.FullSerializer;
+
+public class Destory : MonoBehaviour
+{
+    void Start()
+    {
+        //Remove GameObject
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+}
