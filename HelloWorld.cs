ffcf6da7fee98c04a6129ac8545c665203df0ff9
LavaPremium <maksimlaktionov372@gmail.com>
2025-04-24 17:57:33 +0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Unity.VisualScripting.FullSerializer;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        print("ќтветы\nномер 1 150м");
+        print("номер 2 24 кЌ");
+    }
+
+    void Update()
+    {
+        
+    }
+}
